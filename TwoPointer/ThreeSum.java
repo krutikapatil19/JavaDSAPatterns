8f47552be2e395679514d47dcc039b7cfac2c507
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-15 18:46:45 +0530

    3Sum.(Find Triplets which give a sum result = 0).

--- TwoPointer/ThreeSum.java
+++ TwoPointer/ThreeSum.java
@@ -1,27 +1,38 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 public class ThreeSum {
-    public static int[] findTriplets(int[] nums){
+    public static List<List<Integer>> findTriplets(int[] nums){
 
         Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
         
         for(int i = 0;i<nums.length-2;i++){
+
+            if (i>0){if(nums[i] == nums[i-1]){
+                continue;
+            }
+        }
             int j = i+1;
             int k = nums.length-1;
 
             while(j<k) {
                 int sum = nums[i]+nums[j]+nums[k];
                 if(sum == 0){
-                    return new int[] {nums[i], nums[j],nums[k]};
+                 result.add(Arrays.asList(nums[i],nums[j],nums[k])) ;
+                 j++;
+                 k--;
                 }
+                
                 else if(sum>0){
                     k--;
                 } else j++;
             }
         }
-        return new int[] {-1,-1,-1};
+        return result;
     }
     public static void main(String[] args){
-        int [] nums = {-8,-7,-4,3,6,11,14};
-        System.out.println(Arrays.toString(findTriplets(nums)));
+        int [] nums = {-8,-7,-4,3,6,11,15};
+        System.out.println(findTriplets(nums));
     }
 }

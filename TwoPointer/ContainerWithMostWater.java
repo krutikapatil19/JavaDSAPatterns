4e56719908532c48646e8bebbeea2cf098566b5f
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-16 12:37:00 +0530

    Container With Most Water (Leetcode 11).

---
+++ TwoPointer/ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+public class ContainerWithMostWater {
+    public static int MaxWaterArea(int[] height){
+        int i = 0;
+        int j = height.length-1;
+
+        int maxArea = 0;
+
+        while(i<j){
+            int waterArea = Math.min(height[i],height[j])* (j-i);
+
+            if(waterArea>maxArea){
+                maxArea = waterArea;
+
+            }
+                if(height[i]>height[j]) j--;
+                else i++;
+        }
+             return maxArea;
+    } 
+    public static void main(String[] args){
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(MaxWaterArea(height));
+    }
+}

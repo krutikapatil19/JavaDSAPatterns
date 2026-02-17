a107411245063e0968d48438606ed5bfb6207702
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-17 09:35:59 +0530

    ThreeSumClosest

--- TwoPointer/ThreeSumClosest.java
+++ TwoPointer/ThreeSumClosest.java
@@ -2,24 +2,27 @@ import java.util.Arrays;
 public class ThreeSumClosest {
     public static int FindClosestDifference(int[] nums,int target){
         
-
-        int bestClosestSum = nums[0] + nums[1] + nums[2];
         Arrays.sort(nums);
-        
+        int bestClosestSum = nums[0] + nums[1] + nums[2];
+
         for(int i = 0;i<nums.length-2;i++){
         
         int j = i+1;
         int k = nums.length-1;
 
         while(j<k){
-            
             int sum = nums[i]+nums[j]+nums[k];
+            if(sum==target){
+                return sum;
+            }
 
+            if(Math.abs(sum-target) < Math.abs(bestClosestSum-target)){
+                bestClosestSum = sum;
+            }
+            
             if(sum>target){
                 k--;
             } else j++;
-
-
         }
     }
         return bestClosestSum;

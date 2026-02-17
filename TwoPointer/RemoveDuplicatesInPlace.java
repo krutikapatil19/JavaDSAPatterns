7a8104b9e2da2b8ccd52387b1e38ce34a60dca95
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-17 16:59:15 +0530

    RemoveDuplicates In Place.

--- TwoPointer/RemoveDuplicatesInPlace.java
+++ TwoPointer/RemoveDuplicatesInPlace.java
@@ -1,15 +1,23 @@
 public class RemoveDuplicatesInPlace {
     public static int removeDuplicates(int[] nums){
         
+        //j will point to the position of the last unique element
         int j = 0;
 
         for(int i = 1;i<nums.length;i++){
         
+            //If current element is different from last unique element
             if(nums[i] != nums[j]){
+
+                //Move j forward(expand unique section)
                 j++;
+
+                //copy the new unique element at position j
                 nums[j] = nums[i];
             }
         }
+
+        //return total number of unique elements
         return j+1;
     }
     public static void main(String[] args){

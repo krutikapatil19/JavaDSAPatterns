f4ae9484fae478ee1837936d939095b95ae4ca1f
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-19 13:20:09 +0530

    Move Zeroes To End ,in place.

--- TwoPointer/MoveZeroesToEnd.java
+++ TwoPointer/MoveZeroesToEnd.java
@@ -5,11 +5,14 @@ public class MoveZeroesToEnd{
 
         for(int i = 0;i<nums.length;i++){
             if(nums[i] != 0){
+
+                if(i!=j){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j]=temp;   
-                j++;
             } 
+            j++;
+        }
         }
         return nums;
     }

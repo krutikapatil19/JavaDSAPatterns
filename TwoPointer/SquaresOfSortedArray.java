be660a373cf4b0c1662e1e9e1858069651d025e3
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-19 18:45:35 +0530

    Squares of Sorted Array.

--- TwoPointer/SquaresOfSortedArray.java
+++ TwoPointer/SquaresOfSortedArray.java
@@ -6,25 +6,23 @@ public class SquaresOfSortedArray {
         int j = nums.length-1; //j starts from end
 
         int[] result = new int[nums.length]; //new array to store the sorted squares 
-        int k = result.length-1;
+        int k = result.length-1; //k fills the array from the end 
 
-        while(i<=j){
+        while(i<=j){ //loop until both the pointers meet 
 
             //Calculate squares of both ends
-
             int squareLeft = nums[i] * nums[i];
             int squareRight = nums[j] * nums[j];
 
             //compare both the squares, and place the bigger one at the end of the result
-
             if(squareLeft< squareRight){
-                result[k] = squareRight;
-                j--;
-                k--;
+                result[k] = squareRight;  //put right square
+                j--;  //move right pointer
+                k--;  //decrease k (to the left side)
             } else {
-                result[k] = squareLeft;
-                i++;
-                k--;
+                result[k] = squareLeft; //if left square is greater than the right square , put it at the kth position
+                i++;  //move left pointer 
+                k--;  //move k towards left 
             }
         }
         return result;

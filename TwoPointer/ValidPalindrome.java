ad76de0361e4841ef282a9ab93bb5c5e82fd204e
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-20 19:22:47 +0530

    Valid Palindrome check.

--- TwoPointer/ValidPalindrome.java
+++ TwoPointer/ValidPalindrome.java
@@ -4,21 +4,21 @@ public class ValidPalindrome {
         int i = 0, j= s.length()-1;
 
         while(i<j){
-            while(i<  j && !Character.isLetterOrDigit(s.charAt(i))){
+            while(i<  j && !Character.isLetterOrDigit(s.charAt(i))){        //skip spaces/symbols from left 
                 i++;
             }
 
-            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){          //skip spaces/symbols from right 
                 j--;
             }
 
-            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){       //compares characters from both sides , if they don't match , then return false ,i.e its not palindrome
                 return false;
             }
-            i++;
-            j--;
+            i++;            //move both pointers inward     
+            j--;            //move it inward , if both sides characters keep matching 
         }
-        return true;
+        return true;        //if no mismatch -> then its a palindrome
     }
     public static void main(String[] args){
         String s = "madam";

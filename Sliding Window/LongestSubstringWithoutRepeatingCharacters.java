ee2b2b27f87b6058b59680d776a60d109856af8a
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-21 12:19:23 +0530

    Longest Substring Without Repeating Characters.

--- Sliding Window/LongestSubstringWithoutRepeatingCharacters.java
+++ Sliding Window/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,5 @@
 import java.util.HashMap;
-public class MaxSubstringWithoutRepeatingCharacters {
+public class LongestSubstringWithoutRepeatingCharacters {
     public static int largestUniqueSubstring(String str){
 
         HashMap<Character,Integer> map = new HashMap<>();

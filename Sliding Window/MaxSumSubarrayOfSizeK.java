73438256a4bd7edc6c422439d1e935f3801335db
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-21 11:46:09 +0530

    Maximum sum Subarray of Size K.

--- Sliding Window/MaxSumSubarrayOfSizeK.java
+++ Sliding Window/MaxSumSubarrayOfSizeK.java
@@ -1,18 +1,18 @@
 public class MaxSumSubarrayOfSizeK {
     public static int maximumSubarraySum(int[] nums,int k){
 
-        int currWindowSum  = 0;
-        int maxSum;
+        int currWindowSum  = 0;     //sum of current window 
+        int maxSum;         //best sum found so far 
 
         for(int i = 0;i<k;i++){
-            currWindowSum += nums[i];
+            currWindowSum += nums[i];       //first window becomes starting answer  
         }
 
-        maxSum = currWindowSum;
+        maxSum = currWindowSum;         //assuming the first window as the maxSum 
 
-        for(int i = k;i<nums.length; i++){
-            currWindowSum = currWindowSum - nums[i-k] + nums[i];
-            maxSum = Math.max(currWindowSum, maxSum);
+        for(int i = k;i<nums.length; i++){  //sliding the window 
+            currWindowSum = currWindowSum - nums[i-k] + nums[i];    //removing the leftmost element from the window ,and adding the next one to the rightmost element
+            maxSum = Math.max(currWindowSum, maxSum);       //update max if current window is better or gives maximum sum 
         }
 
         return maxSum;

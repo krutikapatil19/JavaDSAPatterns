1e5d808fd70f12a451a4d707568936c45b1f5625
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-02 19:35:52 +0530

    Three Sum Implementation.

--- TwoPointer/ThreeSum.java
+++ TwoPointer/ThreeSum.java
@@ -25,19 +25,25 @@ public class ThreeSum {
             //Find pair such that pair sum becomes 0
             while(j<k) {
                 int sum = nums[i]+nums[j]+nums[k];
+
                 if(sum == 0){
+                //If sum is 0 , store the Triplet
                  result.add(Arrays.asList(nums[i],nums[j],nums[k])) ;
+
+                 //Move both pointers
                  j++;
                  k--;
                 
-
+                //Skip duplicate values for j
                 while(j < k && nums[j] == nums[j-1]) j++;
+                //Skip duplicate values for k
                 while(k > j && nums[k] == nums[k+1]) k--; 
                 
                 }
+                //If sum is too big , decrease k (towards left)
                 else if(sum>0){
                     k--;
-                } else j++;
+                } else j++; //else move j towards right , so that sum value can increase
 
             }
         }

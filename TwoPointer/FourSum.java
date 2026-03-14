2698b3b07b43e46dec3af005c23821a4afd01958
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-14 17:40:37 +0530

    Four Sum Implementation.

--- TwoPointer/FourSum.java
+++ TwoPointer/FourSum.java
@@ -15,6 +15,7 @@ public class FourSum {
             if(i>0 && nums[i]== nums[i-1]) continue;
 
             for(int j = i+1; j < nums.length-2; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
             
             int k = j+1;
             int l = nums.length -1;

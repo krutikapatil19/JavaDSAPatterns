daa8ad44941d35b2b4c3eac5961c6b5fb6554c13
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-17 15:03:33 +0530

    Shortest Unsorted Continuous Subarray.

--- TwoPointer/ShortestUnsortedContinuousSubarray.java
+++ TwoPointer/ShortestUnsortedContinuousSubarray.java
@@ -2,36 +2,42 @@ public class ShortestUnsortedContinuousSubarray {
     public static int findUnsortedSubarray(int[] nums){
 
         //variables for boundaries
-        int left = -1;
-        int right = -1;
+        int left = -1;          //left -> start of unsorted part 
+        int right = -1;         //right -> end of unsorted part
         
         int length = 0;
 
+        //1: Find first place from where sorting breaks from left 
         for(int i= 0;i<nums.length-1;i++){
             if(nums[i] > nums[i+1]){
-                left = i;
+                left = i;           //unsorted part starts here 
                 break;
             }
         } 
+        
+        //if array is already sorted , return 0
         if(left == -1)return 0;
 
+        //2.Find first place where sorting breaks from right 
         for(int j=nums.length-1;j>0;j--){
             if(nums[j]<nums[j-1]){
-                right = j;
+                right = j;      //unsorted part ends here 
                 break;
             } 
         }
 
+
+        //Step 3: Find min and max in the unsorted window 
         int min = nums[left];
         int max = nums[left];
 
         for(int k = left; k<=right; k++){
             if(nums[k] < min) {
-                min = nums[k];
+                min = nums[k];      //smallest value in window
             }
 
             if(nums[k] > max){
-                max = nums[k];
+                max = nums[k];      //largest value in window 
             }
         }
             while(left > 0 && nums[left - 1] > min){

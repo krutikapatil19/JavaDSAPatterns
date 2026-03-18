72afcbeb9fc9ed57db30a2b1a6d364cb971ce79b
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-18 21:23:44 +0530

    Detect Cycle in LinkedList .

--- FastAndSlowPointers.java/DetectCycle.java
+++ FastAndSlowPointers.java/DetectCycle.java
@@ -21,7 +21,7 @@ public class DetectCycle {
             //slow moves one step and Fast moves two steps at a time.
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == fast) return true;
+            if(slow == fast) return true;   //if slow and fast meets ,it means cycle exists.
         }
         return false;
     }

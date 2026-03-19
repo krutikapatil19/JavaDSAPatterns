2ae7aba09a02bbb5838639063e544f384f0ffeb1
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-19 18:40:47 +0530

    Start Of Linked List .

---
+++ FastAndSlowPointers.java/StartOfLinkedListCycle.java
@@ -0,0 +1,56 @@
+class Node {
+    int val;
+    Node next;
+    Node(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+public class StartOfLinkedListCycle {
+    
+    public static Node startOfCycle (Node head){
+
+        
+        //Fast and slow pointers
+        Node slow = head;
+        Node fast = head;
+
+        while(fast!=null && fast.next != null){
+
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                Node temp = head;
+
+            //moving both 1 step
+            while(temp !=slow){
+            slow = slow.next;
+            temp = temp.next;
+        }
+        return temp;
+    }
+    }
+    return null;
+    }
+    public static void main(String[] args){
+
+        Node n1 = new Node(2);
+        Node n2 = new Node(5);
+        Node n3 = new Node(10);
+        Node n4 = new Node(16);
+        Node n5 = new Node(26);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n3;
+
+        Node result = startOfCycle(n1);
+
+        if(result!=null){
+            System.out.println("Start Of Cycle : " + result.val);
+        }else{
+            System.out.println("No Cycle");
+        }
+    }
+}

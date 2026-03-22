f4422e07a0e964cec81443ef12852e8be4af2f2b
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-22 13:25:07 +0530

    Middle Of The Linked List.

--- FastAndSlowPointers.java/MiddleOfTheLinkedList.java
+++ FastAndSlowPointers.java/MiddleOfTheLinkedList.java
@@ -27,11 +27,13 @@ public class MiddleOfTheLinkedList {
         Node n2 = new Node(5);
         Node n3 = new Node(10);
         Node n4 = new Node(12);
+        Node n5 = new Node(15);
 
 
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
+        n4.next = n5;
 
         System.out.println(FindMiddle(n1).val);
     }

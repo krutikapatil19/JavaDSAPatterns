3887b5e5ab47b505dc9f70d65d21dc583bc6525d
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-26 12:17:43 +0530

    Reorder list.

--- FastAndSlowPointers.java/ReorderLinkedList.java
+++ FastAndSlowPointers.java/ReorderLinkedList.java
@@ -47,7 +47,32 @@ public class ReorderLinkedList {
             first = temp1;
             second = temp2;
         }
-        
+    }
+
+    public static void printList(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args){
+
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        reorderList(n1);
+
+        printList(n1);
     }
 }
 

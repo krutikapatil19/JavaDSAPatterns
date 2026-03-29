a69faff2a53013e78bd00bd7688f6d639e16570b
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-29 11:45:52 +0530

    Min Sum Subarray Length.

--- Sliding Window/MinSumSubarrayOfSizeK.java
+++ Sliding Window/MinSumSubarrayOfSizeK.java
@@ -14,11 +14,11 @@ public class MinSumSubarrayOfSizeK {
                 start++;
             }
         }
-        return minLength;
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
     }
     public static void main(String[] args){
         int[] nums = {2,3,1,2,4,3};
-        int target = 7;
+        int target = 9;
         System.out.println(minimumSubarrayLength(nums,target));
     }
 }

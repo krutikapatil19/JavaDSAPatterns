e013d6eae980458eed7d1bb829c1043ecf50f8ee
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-30 19:08:02 +0530

    Longest Substring with K Unique Characters.

--- Sliding Window/longestSubstringWithKUniqueCharacters.java
+++ Sliding Window/longestSubstringWithKUniqueCharacters.java
@@ -2,22 +2,23 @@ import java.util.HashMap;
 public class longestSubstringWithKUniqueCharacters {
     public static int lengthOfLongestKSubstring(String s, int k){
 
-        int left = 0;
-        int maxLength=0;
+        int left = 0;       //start of window 
+        int maxLength=0;    //length of substring with k distinct chracters
 
-        //using HashMap to store k distinct characters
+        //using HashMap to store frequency
         HashMap<Character,Integer> map = new HashMap<>();
 
         for(int right= 0;right<s.length();right++){
             char ch = s.charAt(right);
+
+            //Add character to window(increse frequency)
             map.put(ch, map.getOrDefault(ch,0) + 1);
         
-        //shrink window if more than k unique characters
-        while(map.size()>k){
+            //shrink window if more than k unique characters
+            while(map.size()>k){
             char leftChar = s.charAt(left);
             map.put(leftChar, map.get(leftChar)-1); //decrease freq
 
-            //shrink window
             //if frequency becomes 0 -> remov from map
             if(map.get(leftChar)==0){
                 map.remove(leftChar);

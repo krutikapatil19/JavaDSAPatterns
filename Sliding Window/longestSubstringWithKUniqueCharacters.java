47a2c713ef36d6671c7885b78fea58b46e680c93
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-30 12:41:59 +0530

    Longest Substring with K Unique Characters.

--- Sliding Window/longestSubstringWithKUniqueCharacters.java
+++ Sliding Window/longestSubstringWithKUniqueCharacters.java
@@ -12,9 +12,7 @@ public class longestSubstringWithKUniqueCharacters {
         for(right= 0;right<s.length();right++){
             char ch = s.charAt(right);
             map.put(ch, map.getOrDefault(ch,0) + 1);
-            //we will take 3 starting chracters for this , add the 4th character ,and checking if all 4 are distinct 
-            //length = j-i+1;
-
+        
         while(map.size()>k){
             char leftChar = s.charAt(left);
             map.put(leftChar, map.get(leftChar)-1); //decrease freq

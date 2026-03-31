7bd4c6107e675ee2cf9c4ef84558a16ee57cbdcc
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-03-31 23:16:27 +0530

    Fruits Into Baskets problem.

--- Sliding Window/FruitsIntoBaskets.java
+++ Sliding Window/FruitsIntoBaskets.java
@@ -40,7 +40,7 @@ public class FruitsIntoBaskets {
         System.out.println(countOfFruits(fruits));
     }
 }
-
-
+// We expand the window using right pointer
 // Add fruit → shrink if >2 types → update max
+// If more than 2 fruit types, shrink using left pointer
 // Sliding window ensures optimal O(n) solution

1f7bfd321062e77e3cfc80b1b29aaffa329d7db3
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-02 11:23:32 +0530

    No Repeat Substring.0

---
+++ Sliding Window/NoRepeatSubstring.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+public class NoRepeatSubstring {
+    public static int longestSubstringWithoutRepeatingChar(String s){
+
+        int left = 0;
+        int length = 0;
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(int right = 0; right<s.length(); right++){
+            char ch = s.charAt(right);
+            
+            map.put(ch, map.getOrDefault(ch,0) + 1);
+
+            while(map.get(ch)>1){
+                char leftChar = s.charAt(left);
+
+                map.put(leftChar, map.get(leftChar)-1);
+
+                //shrinking the window logic 
+
+                if(map.get(leftChar)==0){
+                    map.remove(leftChar);
+                }
+                left++;
+            }
+            length = Math.max(length, right-left+1);
+        }
+    return length;
+    }
+    public static void main(String[] args){
+        String s = "abcabb";
+        System.out.println(longestSubstringWithoutRepeatingChar(s));
+    }
+}

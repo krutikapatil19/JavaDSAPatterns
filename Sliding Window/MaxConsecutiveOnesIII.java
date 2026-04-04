88a332009e33ba032bf8a1f3669611fd0f83e500
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-04 18:41:05 +0530

    Max Consecutive Ones III.

--- Sliding Window/MaxConsecutiveOnesIII.java
+++ Sliding Window/MaxConsecutiveOnesIII.java
@@ -1,23 +1,30 @@
 public class MaxConsecutiveOnesIII {
     public static int maximumOnes(int[] arr, int k){
 
-        int left = 0;
-        int zeroesCount = 0;
-        int maxLen = 0;
+        int left = 0;           //start of window
+        int zeroesCount = 0;    //number of 0s in current window 
+        int maxLen = 0;         //answer (max length found)
 
+        //move right pointer
         for(int right = 0;right<arr.length;right++){
-            //if current element is 0 → zeroesCount++
-           
+
+            //if current element is 0 → increase zero count
             if(arr[right] == 0) {
                 zeroesCount++;
             }
 
+            //if window becomes invalid (more than k zeroes)
             while(zeroesCount >k) {
+
+                //if left element is 0 -> reduce zero count
                 if(arr[left] ==0) {
                     zeroesCount--;
                 }
+
+                //shrink window 
                 left++;
             }
+            //update max length
             maxLen = Math.max(maxLen, right - left + 1);
         }
         return maxLen;

b57d5a071fc0d1168390e6b6028c6d71efa90d49
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-04 21:30:07 +0530

    Minimum Subarray Sum Length.

---
+++ Sliding Window/MinimumSubarraySumLen.java
@@ -0,0 +1,27 @@
+public class MinimumSubarraySumLen {
+    public static int minimumLengthSubarray(int[] arr,int target){
+
+        int left = 0;
+        int minLength = Integer.MAX_VALUE;
+        int targetSum = 0;
+
+        for(int right = 0;right<arr.length;right++){
+
+           targetSum += arr[right];
+
+        while(targetSum>=target){
+            minLength = Math.min(minLength, right-left+1);
+    
+            targetSum -= arr[left];
+            left++;
+        }
+    }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+        public static void main(String[] args){
+            int[] arr = {2,3,1,2,4,3};
+            int target = 7;
+            System.out.println(minimumLengthSubarray(arr,target));
+        }
+    }
+

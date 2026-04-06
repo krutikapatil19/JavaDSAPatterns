175da36ca6dec6f38c2dc4d17aee5186ef420c4d
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-06 12:50:27 +0530

    Minimum Window Substring .

--- Sliding Window/MinimumWindowSubstring.java
+++ Sliding Window/MinimumWindowSubstring.java
@@ -1,55 +1,56 @@
 import java.util.HashMap;
+
 public class MinimumWindowSubstring {
-    public static String minWindow(String s, String t){
+    public static String minWindow(String s, String t) {
 
-        int left = 0;                           //start of window
-        int formed = 0;                         //how many characters matched
-        int minLength = Integer.MAX_VALUE;      //smallest window length
-        int startIndex = 0;                     
-        
-        HashMap<Character, Integer> need = new HashMap<>();         //what we need
-        HashMap <Character,Integer> window = new HashMap<>();       //what we have 
+        int left = 0; // start of window
+        int formed = 0; // how many characters matched
+        int minLength = Integer.MAX_VALUE; // smallest window length
+        int startIndex = 0;
 
-        //Build frequency map for String t
-            for(char ch : t.toCharArray()){
-                need.put(ch, need.getOrDefault(ch, 0) + 1);
+        HashMap<Character, Integer> need = new HashMap<>(); // what we need
+        HashMap<Character, Integer> window = new HashMap<>(); // what we have
+
+        // Build frequency map for String t
+        for (char ch : t.toCharArray()) {
+            need.put(ch, need.getOrDefault(ch, 0) + 1);
         }
-            int required = need.size();     //number of unique characters required
+        int required = need.size(); // number of unique characters required
 
-        //Traverse string s using sliding window 
-        for(int right = 0;right<s.length();right++){
+        // Traverse string s using sliding window
+        for (int right = 0; right < s.length(); right++) {
 
-            //taking current char
+            // taking current char
             char ch = s.charAt(right);
-            window.put(ch, window.getOrDefault(ch, 0) + 1); 
+            window.put(ch, window.getOrDefault(ch, 0) + 1);
 
-            if(need.containsKey(ch) && window.get(ch).intValue() == need.get(ch).intValue()){
+            if (need.containsKey(ch) && window.get(ch).intValue() == need.get(ch).intValue()) {
                 formed++;
             }
-        
-        while(formed == required){
 
-            //update smallest window 
-            if(right - left + 1 <minLength){
-                minLength = right - left + 1;
-                startIndex = left;
+            while (formed == required) {
+
+                // update smallest window
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    startIndex = left;
+                }
+                // removing left character(shrink window)
+                char leftChar = s.charAt(left);
+                window.put(leftChar, window.get(leftChar) - 1);
+
+                if (need.containsKey(leftChar) && window.get(leftChar) < need.get(leftChar)) {
+                    formed--;
+                }
+                left++; // move left pointer
             }
-            //removing left character(shrink window)
-            char leftChar = s.charAt(left);
-            window.put(leftChar, window.get(leftChar) - 1);
-
-
-            if(need.containsKey(leftChar) && window.get(leftChar) < need.get(leftChar)){
-                formed--;
-            }
-            left++;     //move left pointer
         }
+        return minLength == Integer.MAX_VALUE ? "" : s.substring(startIndex, startIndex + minLength);
     }
-    return minLength == Integer.MAX_VALUE ? "" : s.substring(startIndex, startIndex + minLength);
-    }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         String s = "ADOBECODEBANC";
         String t = "ABC";
-        System.out.println(minWindow(s,t));
+        System.out.println(minWindow(s, t));
     }
 }

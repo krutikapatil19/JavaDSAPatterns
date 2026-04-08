6c9a704dd03cf67721330dffef622a24ca923644
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-08 12:27:11 +0530

    Minimum Subarray Sum.

---
+++ Kadane Pattern/MinimumSubarraySum.java
@@ -0,0 +1,20 @@
+public class MinimumSubarraySum {
+    public static int minimumSubarraySum(int[] nums){
+        int minSum = Integer.MAX_VALUE;
+        int currSum= nums[0];
+        for(int i = 0;i<nums.length;i++){
+            currSum += nums[i];
+
+            minSum = Math.min(currSum, minSum);
+
+            if(currSum>0){
+                currSum = 0;
+            }
+        }
+        return minSum;
+    }
+    public static void main(String[] args){
+        int[] nums = {3,-4, 2,-3,-1, 7,-5};
+        System.out.println(minimumSubarraySum(nums));
+    }
+}

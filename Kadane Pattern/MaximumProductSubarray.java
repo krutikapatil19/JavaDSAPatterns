f81da4a3c64174d8e73cf7df77aafed7888f9efb
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-08 17:30:27 +0530

    Maximum Product Subarray .

--- Kadane Pattern/MaximumProductSubarray.java
+++ Kadane Pattern/MaximumProductSubarray.java
@@ -9,11 +9,15 @@ public class MaximumProductSubarray {
             int prevMax = currMax;      //store old max
             int prevMin = currMin;      //store old min
 
+            /*try 3 options : 
+            1.Take current element alon
+            2.Multiply with prev max
+            3.multiply with prev min (imp for negative flip) */
             currMax = Math.max(nums[i], Math.max( nums[i]*prevMax, nums[i]*prevMin));
 
             currMin = Math.min(nums[i], Math.min( nums[i]*prevMax, nums[i]*prevMin));
 
-            maxProduct = Math.max(maxProduct, currMax);     //comparison between maxProduct and currMax
+            maxProduct = Math.max(maxProduct, currMax);     //comparison between maxProduct and currMax and update the final answer(that produces the maximum product)
 
         }
         return maxProduct;

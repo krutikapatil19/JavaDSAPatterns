ff192499c4d99581a5d9167ced6b91287d830f0b
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-08 12:14:25 +0530

    Maximum Subarray Sum.

--- Kadane Pattern/MaximumSubarraySum.java
+++ Kadane Pattern/MaximumSubarraySum.java
@@ -1,16 +1,19 @@
 public class MaximumSubarraySum {
     public static int maximumSumSubarray (int[] nums){
 
-        int currSum = nums[0];
-        int maxSum = Integer.MIN_VALUE;
+        int currSum = nums[0];          //current running sum of subarray
+        int maxSum = Integer.MIN_VALUE;     //stores best (maximum) sum found so far 
 
         for(int i = 0;i<nums.length;i++){
-            currSum += nums[i];
+            currSum += nums[i];     //add current element to running sum
 
+            //update maxSum if currentSum is better
             maxSum = Math.max(currSum , maxSum);
 
+            //if running sum becomes negative -> drop it  , because negative sum will only decrease future results 
+
             if(currSum < 0){
-                currSum = 0;
+                currSum = 0;    //restart from the next element
             }
         }
         return maxSum;

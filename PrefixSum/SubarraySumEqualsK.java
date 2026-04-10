0d605c8ab2097f5c305ccdd924928d1ac22e554f
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-10 18:56:20 +0530

    Subarray Sum Equals K.

--- PrefixSum/SubarraySumEqualsK.java
+++ PrefixSum/SubarraySumEqualsK.java
@@ -5,11 +5,11 @@ public class SubarraySumEqualsK {
 
     for(int i = 0; i<nums.length; i++){
 
-        int sum = 0;
+        int sum = 0;    //reset for each i
 
         for(int j = i; j<nums.length; j++){
 
-                sum += nums[j];
+                sum += nums[j];     //reuse previous sum
 
                 if(sum == k){
                     count++;

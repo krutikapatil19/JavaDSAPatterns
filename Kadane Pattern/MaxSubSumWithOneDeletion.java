2e58f2052f47a316ad7a5ebb2b0d6aa0b4060050
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-10 09:56:33 +0530

    Max Subarray Sum With One Deletion.

--- Kadane Pattern/MaxSubSumWithOneDeletion.java
+++ Kadane Pattern/MaxSubSumWithOneDeletion.java
@@ -9,14 +9,16 @@ public class MaxSubSumWithOneDeletion {
 
             int prevNoDeletion = noDeletionSum; //store old value before update 
 
-
             /*2 Choices: 
             1.continue after deletion
             2.delete current element (take prev noDeletion)
             */
             oneDeletionSum = Math.max(oneDeletionSum + nums[i], prevNoDeletion);
+
+            //Normal Kadane (no deletion case)
             noDeletionSum = Math.max(nums[i], noDeletionSum + nums[i]);
 
+            //Update final answer from both
             maxSum = Math.max(maxSum, Math.max(noDeletionSum, oneDeletionSum));
         }
         return maxSum;

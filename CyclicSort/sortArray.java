aa537b927f72fed15926ec1552183336992d7cf9
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-22 21:46:44 +0530

    Cyclic Sort.

---
+++ CyclicSort/sortArray.java
@@ -0,0 +1,29 @@
+public class sortArray {
+    
+    public static void sortTheArray(int[] arr) {
+        int i = 0;
+
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+
+           
+            if (arr[i] != arr[correctIndex]) {
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            } else {
+                i++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 4, 2};
+
+        sortTheArray(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+

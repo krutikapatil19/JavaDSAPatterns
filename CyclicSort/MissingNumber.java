5fbdd1fe81b1ee44113fc9fd7d27ccb9e86b71b6
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-24 23:06:31 +0530

    Find Missing Number.

--- CyclicSort/MissingNumber.java
+++ CyclicSort/MissingNumber.java
@@ -1,5 +1,18 @@
 public class MissingNumber {
-    public static int findMissingNumber(int[] nums){
+    public static int findMissingNumber(int[] nums,int n){
+         n = nums.length;
         
+            int totalSum = n*(n+1)/2;
+
+            int missingNumber = totalSum - _;
+
+
     }
+    return missingNumber;
+
+    public static void main(String[] args){
+        int n = 3;
+        int[] nums = {3,0,1};
+    System.out.println(findMissingNumber(nums,n));
+}
 }

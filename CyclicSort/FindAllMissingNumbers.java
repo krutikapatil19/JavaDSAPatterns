4c85122d33025187aef1d3573b740ae0686707b7
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-25 13:02:23 +0530

    Find All Missing Numbers.

--- CyclicSort/FindAllMissingNumbers.java
+++ CyclicSort/FindAllMissingNumbers.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 public class FindAllMissingNumbers {
-    
+    public static List<Integer> sortingArray(int[] nums){
+        int i = 0;
+
+        List<Integer> result = new ArrayList<>();
+        while(i<nums.length){
+            int correctIndex = nums[i] -1 ;
+
+            if(nums[i] != nums[correctIndex]) {
+                //swap
+
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            } else {
+                i++;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] nums = {4,3,2,7,8,2,3,1};
+        System.out.print(sortingArray(nums));
+    }
 }
+
+
+//cyclic sort approach 

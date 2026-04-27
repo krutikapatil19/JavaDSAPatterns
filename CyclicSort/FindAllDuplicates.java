4f873fc4e950d0b1c1b626c1c144d06d26fcaed1
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-04-27 10:54:46 +0530

    Find All Duplicates.

---
+++ CyclicSort/FindAllDuplicates.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.ArrayList;
+public class FindAllDuplicates {
+    public static List<Integer> findingAllDuplicates(int[] nums){
+
+        int i = 0;
+
+        List<Integer> result = new ArrayList<>();
+
+        while(i < nums.length){
+            
+            int correctIndex = nums[i] - 1;
+
+            if(nums[i] != nums[correctIndex]) {
+
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            } else {
+                i++;
+            }
+
+            for(int j = 0; j< nums.length; j++){
+                if(nums[j] != j + 1){
+                    result.add(nums[j]);
+                }
+            }
+        }
+            return result;
+        }
+    public static void main(String[] args){
+        int[] nums = {4,3,2,7,8,2,3,1};
+        System.out.println(findingAllDuplicates(nums));
+    }
+}
+

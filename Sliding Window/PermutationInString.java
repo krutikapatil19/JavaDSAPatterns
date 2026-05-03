f47cd771e9f8dcb986a0a013ba70f61b20947a1f
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-03 12:38:29 +0530

    Permutation In String.

--- Sliding Window/PermutationInString.java
+++ Sliding Window/PermutationInString.java
@@ -1,23 +1,28 @@
 public class PermutationInString {
     public static boolean checkInclusion (String s1, String s2){
 
+        //if s1 is bigger - > inclusion is not possible in s2
         if(s1.length()>s2.length()){
             return false;
         }
 
-        //Freq arrays
-        int[] need = new int[26]; 
-        int[] window = new int[26];
+        //Freqency arrays
+        int[] need = new int[26];       //freq of s1
+        int[] window = new int[26];     //freq of current window in s2
 
+        //Fill need array from s1
         for(char c : s1.toCharArray()) {
             need[c - 'a']++;
         }
 
-        int k = s1.length();
+        int k = s1.length();        //window size
+
+        //build first window
         for(int i = 0; i<k; i++){
             window[s2.charAt(i) - 'a']++;
         }
 
+        //Check first window
         if(matches(need, window)) return true;
 
         for(int i = k; i<s2.length(); i++){

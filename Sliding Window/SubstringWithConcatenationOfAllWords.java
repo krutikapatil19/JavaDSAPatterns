06faadeb32482f02c79600ddf32a09dd5d2403ed
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-04 11:48:30 +0530

    Substring With Concatenation Of All Words.

---
+++ Sliding Window/SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubstringWithConcatenationOfAllWords {
+    public List<Integer> findSubstring (String s1, String[] words){
+
+        List<Integer> result = new ArrayList<>();
+
+        if(s.length()== 0|| words.length == 0) return result;
+
+
+    } 
+    public static void main(String[] args){
+        String s1 = "barfoothefoobarman";
+        String[] words = ["foo","bar"];
+
+        System.out.println(findSubstring(s1, words));
+    }
+}

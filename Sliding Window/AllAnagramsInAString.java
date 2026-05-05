984a6f5dacc181a888812b2632cad133460439a5
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-05 12:39:06 +0530

    All Anagrams In a  String.

---
+++ Sliding Window/AllAnagramsInAString.java
@@ -0,0 +1,10 @@
+public class AllAnagramsInAString {
+    public static List<String> findAnagrams(String s, String p){
+
+    }
+    public static void main(String[] args){
+        String s = "cbaebabacd";
+        String p = "abc";
+        System.out.println(findAnagrams(s,p));
+    }
+}

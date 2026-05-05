fd89b72a08243d8ae74dbbc42e87064b78f271e0
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-05 18:23:39 +0530

    All Anagrams In a  String.

--- Sliding Window/AllAnagramsInAString.java
+++ Sliding Window/AllAnagramsInAString.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
 public class AllAnagramsInAString {
     public static List<String> findAnagrams(String s, String p){
 
+        List<Integer> result = new ArrayList<>();
+
+        if(s.length() < p.length()) return result;
+
+        int[] need = new int[26];
+        int[] window = new int[26];
+
+        for(char c: p.toCharArray()){
+            need[c-'a']++;
+        }
+
+        int k = p.length();
+
+        for(int i = 0;i<k;i++){
+            window[s.charAt(i) - 'a']++;
+        }
     }
     public static void main(String[] args){
         String s = "cbaebabacd";

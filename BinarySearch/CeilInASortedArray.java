6c7a2fd62189aaa315ae0d4696875ac0ed8219d7
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-07 19:19:02 +0530

    Ceil In A Sorted Array.

--- BinarySearch/CeilInASortedArray.java
+++ BinarySearch/CeilInASortedArray.java
@@ -1,13 +1,21 @@
 public class CeilInASortedArray {
     public int static findCloseMaxNumber(int[] nums, int target){
 
+        int ans = -1;
+
         int left = 0;
         int right = nums.length-1;
-        int mid = left+right/2;
+        int mid = left+ (right-left)/2;
 
         while(left<right){
-            
+
+            if(nums[mid] >= target){
+                return mid;
+            } else {
+                right = mid -1;
+            }
         }
+        return -1;
     }
     public static void main(String[] args){
         int[] nums = {1, 2, 8, 10, 11, 12, 19};

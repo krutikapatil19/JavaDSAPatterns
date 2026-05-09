0c3dce759801c50eebe3cc5e263fc7241f9ecab9
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-09 19:57:49 +0530

    First and Last Position Of Element In Sorted Array.

--- BinarySearch/FirstAndLastPositionOfElementInSortedArray.java
+++ BinarySearch/FirstAndLastPositionOfElementInSortedArray.java
@@ -10,7 +10,6 @@ public class FirstAndLastPositionOfElementInSortedArray {
 
         return new int[]{first, last};
     }
-
     public static int binarySearch(int[] nums, int target, boolean findFirst) {
 
         int left = 0;
@@ -36,12 +35,10 @@ public class FirstAndLastPositionOfElementInSortedArray {
                     left = mid + 1;
                 }
             }
-
             //move left
             else if(nums[mid] < target){
                 left = mid + 1;
             }
-
             else {
                 right = mid - 1;
             }

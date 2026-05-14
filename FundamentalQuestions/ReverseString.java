c5401f86eef51dcde55f81a3cc030cc92af48cc0
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-14 12:21:31 +0530

    reverse String.

--- FundamentalQuestions/ReverseString.java
+++ FundamentalQuestions/ReverseString.java
@@ -1,10 +1,12 @@
 public class ReverseString {
     public static String reverse(String s){
         
+        //converting the string to char array , so each character can be accessed independently
         char[] arr = s.toCharArray();
         int i = 0;
         int j = s.length() - 1;
 
+        //used two pointers , i and j , i will start from beginning and j will start from the end , and run the while loop till i crosses j , and along with that , swapping arr[i] and arr[j].
         while(i<j){
            char temp = arr[i];
            arr[i] = arr[j];
@@ -12,7 +14,9 @@ public class ReverseString {
            i++;
            j--;
         }
-        return new String(arr);
+
+        //convert the char array back to String ,and return .
+        return new String(arr);         
     }
     public static void main(String[] args){
         String s = "Programming";

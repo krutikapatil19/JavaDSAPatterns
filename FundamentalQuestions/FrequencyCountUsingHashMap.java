959461104288bec6b94f0aea0fe11b6b606a400b
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-15 17:30:58 +0530

    Frequency Count Using HashMap.

--- FundamentalQuestions/FrequencyCountUsingHashMap.java
+++ FundamentalQuestions/FrequencyCountUsingHashMap.java
@@ -1,13 +1,21 @@
 import java.util.HashMap;
 import java.util.Map;
 public class FrequencyCountUsingHashMap {
+
+    //Function to count frequency of elements in an array 
     public static Map<Integer, Integer> countFrequency(int[] nums){
 
+        //creating a new HashMap object named freqCount , which stores key-value pairs.
         Map<Integer, Integer> freqCount = new HashMap<>();
 
+        //Loop through each element in array (we can also use modified for loop)
         for(int i = 0; i<nums.length; i++){
+
+            //if number already exists , increase count by 1 
             if(freqCount.containsKey(nums[i])){
                 freqCount.put(nums[i], freqCount.get(nums[i]) + 1);
+
+                //if number doesnt exist in map , then put 1.
             } else {
                 freqCount.put(nums[i], 1);
             }

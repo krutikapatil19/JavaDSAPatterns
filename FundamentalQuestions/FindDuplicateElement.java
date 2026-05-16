36095950e5f28a92f715db5a52da9473680388bf
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-16 09:40:59 +0530

    Find All Duplicates.

--- FundamentalQuestions/FindDuplicateElement.java
+++ FundamentalQuestions/FindDuplicateElement.java
@@ -2,7 +2,6 @@ import java.util.HashSet;
 public class FindDuplicateElement {
     public static HashSet<Integer> duplicateNumber(int[] nums){
 
-
         //Set to store already seen elements
         HashSet<Integer> set = new HashSet<>();
 
@@ -12,13 +11,18 @@ public class FindDuplicateElement {
         //Loop through each element in array 
         for(int i = 0;i<nums.length; i++){
 
+            //If element already exists in set , then it is duplicate
             if(set.contains(nums[i])){
+
+                //add duplicate element into duplicates set
                 duplicates.add(nums[i]);
             } else {
+
+                //if number is not present, add it into set
                 set.add(nums[i]);
             }
         }
-        return duplicates;
+        return duplicates;  //return all duplicates 
     }
     public static void main(String[] args){
         int[] nums = {3,5,9,9,3,11};

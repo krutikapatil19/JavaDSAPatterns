6c93eab66397521af7c5b6a80c2bcb526f808b31
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-16 10:57:05 +0530

    Count Vowels in a String.

--- FundamentalQuestions/CountVowels.java
+++ FundamentalQuestions/CountVowels.java
@@ -1,16 +1,24 @@
 public class CountVowels {
+
+    //Function to count vowels in a String
     public static int vowelsCount(String s){
 
         //counter initialization - to keep count of vowels
         int count = 0;
 
+        //Loop through each character in string
         for(int i = 0;i<s.length();i++){
 
             char ch = s.charAt(i);
+
+            //check if character is vowel
             if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                
+                //increase count if vowel found
                 count++;
             }
         }
+        //return total number of vowels
         return count;
     }
     public static void main(String[] args){

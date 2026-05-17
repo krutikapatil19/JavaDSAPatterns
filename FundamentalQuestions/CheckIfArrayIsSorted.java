f02a86e7f548ad2c778c8d94ec34b236d25eadf3
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-17 17:12:40 +0530

    Check if array is Sorted.

--- FundamentalQuestions/CheckIfArrayIsSorted.java
+++ FundamentalQuestions/CheckIfArrayIsSorted.java
@@ -1,11 +1,16 @@
 public class CheckIfArrayIsSorted {
     public static boolean isSorted(int[] nums){
 
+        //Loop through each element till second last index (compare current element with the next one , thats y skipping the last one in loop)
         for(int i = 0; i<nums.length-1; i++){
+            //check if current number is greater than next element
             if(nums[i]>nums[i+1]){
+                
+                //if order breaks, array is not sorted
                 return false;
             }
         }
+        //if loop ends, and no order breaks, then array is sorted.
         return true;
     }
     public static void main(String[] args){

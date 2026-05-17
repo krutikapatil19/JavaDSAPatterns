0986f5ea96a7530e1328b06bf205abd70581c5ca
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-17 09:46:25 +0530

    Sum Of All Elements.

--- FundamentalQuestions/LargestSumInArray.java
+++ FundamentalQuestions/LargestSumInArray.java
@@ -1,5 +1,5 @@
 public class LargestSumInArray {
-    public static int findLargestSum(int[]nums){
+    public static int SumOfAllElements(int[]nums){
 
         int sum = 0;
         for(int i = 0;i<nums.length; i++){
@@ -9,6 +9,6 @@ public class LargestSumInArray {
     }
     public  static void main(String[]args){
         int[] nums = {10,2,4,20};
-        System.out.println(findLargestSum(nums));
+        System.out.println(SumOfAllElements(nums));
     }
 }

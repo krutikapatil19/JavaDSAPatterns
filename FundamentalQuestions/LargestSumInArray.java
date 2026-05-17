b010cc2b0fed38c854c8ca159dd87c17d03402cf
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-17 09:20:00 +0530

    Largest sum in Array.

---
+++ FundamentalQuestions/LargestSumInArray.java
@@ -0,0 +1,14 @@
+public class LargestSumInArray {
+    public static int findLargestSum(int[]nums){
+
+        int sum = 0;
+        for(int i = 0;i<nums.length; i++){
+            sum+= nums[i];
+        }
+        return sum;
+    }
+    public  static void main(String[]args){
+        int[] nums = {10,2,4,20};
+        System.out.println(findLargestSum(nums));
+    }
+}

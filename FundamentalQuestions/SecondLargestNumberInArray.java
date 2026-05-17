cea5376bdb58b569021f644f255942eadcc9b059
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-17 13:08:15 +0530

    Second Largest Number in Array.

--- FundamentalQuestions/SecondLargestNumberInArray.java
+++ FundamentalQuestions/SecondLargestNumberInArray.java
@@ -4,12 +4,18 @@ public class SecondLargestNumberInArray{
 
         int max = Integer.MIN_VALUE;        //variable to store largest number 
         int secondMax = Integer.MIN_VALUE;      //variable to store secondLargest number 
-        
+
+        //Loop through each element in array
         for(int i = 0;i<nums.length; i++){
         
+            //If current element is greater than max, old max becomes SecondMax and max and max gets updated by current number
             if(nums[i]>max){
                 secondMax = max;
+
+                //update max with current element
                 max = nums[i];
+
+                //check if current element can become second Largest (if its lesser than max and greater than secondMax).
             } else if(nums[i]<max && nums[i]>secondMax){
                 secondMax = nums[i];
             }

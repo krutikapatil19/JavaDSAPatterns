11bb676c7424943507013573e487bb01f81e3a7d
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-18 19:10:15 +0530

    Move all Zeroes to End.

--- FundamentalQuestions/MoveAllZeroesToEnd.java
+++ FundamentalQuestions/MoveAllZeroesToEnd.java
@@ -3,15 +3,19 @@ public class MoveAllZeroesToEnd{
 
     public static int[] movingZeroestoEnd(int[]nums){
 
+        //Using two-pointer approach , i keeps track of zeroes elements , and j points to where the next non-zero element should go.
         int i = 0;
         int j = 0;
 
         for(i = 0;i<nums.length; i++){
+
+            //if current number is non zero , then assigning it to jth position, and then incrementing j pointer 
             if(nums[i]!= 0){
                 nums[j]=nums[i];
                 j++;
             } 
         }
+        //In the end , filling the remaining indices with 0 , becuase all the non-zeroes are moved forward.
         for( i = j; i<nums.length; i++){
             nums[i]=0;
         }

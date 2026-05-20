67d4ed3ead3d269d67c5610343406613c688d856
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-20 11:03:09 +0530

    Valid Angram check.

--- FundamentalQuestions/ValidAnagram.java
+++ FundamentalQuestions/ValidAnagram.java
@@ -18,7 +18,11 @@ public class ValidAnagram {
         }
 
         for(char ch : s2.toCharArray()) {
-            if(!map.containsKey(ch)) return fasle;
+            if(!map.containsKey(ch)) return false;
+            map.put(ch, map.get(ch) - 1);
+            if(map.get(ch)==0){
+                map.remove(ch);
+            }
         }
 
         return true;

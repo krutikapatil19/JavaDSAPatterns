e3bd24a7e39d4badd0c923cb355e7fd86553e85e
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-23 22:37:12 +0530

    First and Last Occurence of an Element in Array.

--- FundamentalQuestions/FindFirstAndLastPosition.java
+++ FundamentalQuestions/FindFirstAndLastPosition.java
@@ -23,8 +23,10 @@ public class FindFirstAndLastPosition {
                 lastOccurence = i;
             }
         }
+
         //store first and last occurrence in array
         int[] result = new int []{firstOccurence, lastOccurence};
+        
         //return result array
         return result;
     }

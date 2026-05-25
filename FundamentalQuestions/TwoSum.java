cc2f52e1e859958fdd37a8f714b580bac21e1c6c
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-25 21:52:51 +0530

    Two Sum.

--- FundamentalQuestions/TwoSum.java
+++ FundamentalQuestions/TwoSum.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 public class TwoSum {
     public static int[] findNumbers(int[]nums, int target){
@@ -18,6 +19,6 @@ public class TwoSum {
     public static void main(String[] args){
         int[] nums = {2, 7, 11, 15};
         int target = 18;
-        System.out.println(findNumbers(nums, target));
+        System.out.println(Arrays.toString(findNumbers(nums, target)));
     }
 }

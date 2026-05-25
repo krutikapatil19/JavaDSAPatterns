aa1328132ed7609fea0e89e13a9b287e73b189d8
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-25 21:14:58 +0530

    Two Sum.

---
+++ FundamentalQuestions/TwoSum.java
@@ -0,0 +1,10 @@
+public class TwoSum {
+    public static int[] findNumbers(int[]nums, int target){
+
+    }
+    public static void main(String[] args){
+        int[] nums = {2, 7, 11, 15};
+        int target = 18;
+        System.out.println(findNumbers(nums, target));
+    }
+}

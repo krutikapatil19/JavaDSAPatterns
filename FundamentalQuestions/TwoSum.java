9ce22bc2b2147359f966c41b5274e835225f454f
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-05-25 21:51:21 +0530

    Two Sum.

--- FundamentalQuestions/TwoSum.java
+++ FundamentalQuestions/TwoSum.java
@@ -1,6 +1,18 @@
+import java.util.HashMap;
 public class TwoSum {
     public static int[] findNumbers(int[]nums, int target){
 
+        HashMap <Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0;i<nums.length; i++){
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement)){
+                map.put(nums[i],i);
+            }
+        }
+        return new int[]{};
+        
     }
     public static void main(String[] args){
         int[] nums = {2, 7, 11, 15};

640804409479d26dfa495c86b5320b18b29f504e
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-14 09:25:03 +0530

    Two Sum

--- FundamentalQuestions/prrrr.java
+++ FundamentalQuestions/prrrr.java
@@ -4,16 +4,17 @@ public class prrrr {
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        int[] result = {};
+        int[] result = new int[2];
         for(int i = 0; i<nums.length; i++){
             int complement = target - nums[i];
 
             if(map.containsKey(complement)){
+                result[0] = map.get(complement);
+                result[1] = i;
                 
             } else {
                 map.put(nums[i],i);
             }
-            result = {i, nums[complement]};
         }
         return  result;
     }

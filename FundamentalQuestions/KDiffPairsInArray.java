d14193c43fde9663d1659913c114570ca81bb090
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-15 19:56:48 +0530

    K Diff Pairs in Array.

--- FundamentalQuestions/KDiffPairsInArray.java
+++ FundamentalQuestions/KDiffPairsInArray.java
@@ -4,18 +4,28 @@ import java.util.HashSet;
 public class KDiffPairsInArray {
     public static int findKDiffPairs(int[]nums , int target){
 
+        //store visited numbers and their indices
         HashMap<Integer,Integer> map = new HashMap<>();
+        //Store unique pairs(it avoids dupplicate counting)
         HashSet<String> pairs = new HashSet<>();
+
+        // Stores final count of unique pairs havig k difference
         int count = 0;
+
         for(int i = 0; i<nums.length; i++){
+
+            //Find number whose difference with current element is k
             int complement = nums[i]-target;
 
+            //Check if smaller number already exists
             if(map.containsKey(complement)){
 
                 int smaller = Math.min(nums[i],complement);
                 int larger = Math.max(nums[i],complement);
 
                 String pair = smaller + "," +  larger;
+
+                // Count only if pair is not counted before
                 if(!pairs.contains(pair)){
                 pairs.add(pair);
                 count++;
@@ -26,13 +36,16 @@ public class KDiffPairsInArray {
                 int larger = Math.max(nums[i], nums[i]+target);
 
                 String pair = smaller + "," + larger;
+
+                // Count only if pair is not counted before
                 if(!pairs.contains(pair)){
                     pairs.add(pair);
                     count++;
                 }
             }
+
+            //Store current number for future elements
                 map.put(nums[i],i);
-            
         }
         return count;
     }

54b1f54e99c88c1cdd5496d9dd5dbc221c817c5d
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-17 19:57:00 +0530

    Contains Duplicate.

--- FundamentalQuestions/containsDuplicate.java
+++ FundamentalQuestions/containsDuplicate.java
@@ -2,12 +2,16 @@ import java.util.HashSet;
 public class containsDuplicate {
     public static boolean isDuplicate(int[]nums){
 
+        //HashSet stores unique elements (avoids duplicates)
         HashSet<Integer> set = new HashSet<>();
 
             for(int i = 0; i<nums.length; i++){
+            //Number already seen before -> duplicate found - return true
             if(set.contains(nums[i])){
                 return true;
             } else {
+
+                //if set doesnt contain number , then add it into the set.
                 set.add(nums[i]);
             }
         }

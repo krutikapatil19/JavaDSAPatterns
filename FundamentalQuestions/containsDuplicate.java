3044a8cedb3b9c18a5209f5e772a4b79a0a6de1a
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-17 19:49:05 +0530

    Contains Duplicate.

--- FundamentalQuestions/containsDuplicate.java
+++ FundamentalQuestions/containsDuplicate.java
@@ -1,13 +1,14 @@
-import java.util.HashMap;
+import java.util.HashSet;
 public class containsDuplicate {
     public static boolean isDuplicate(int[]nums){
 
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
+        HashSet<Integer> set = new HashSet<>();
+
+            for(int i = 0; i<nums.length; i++){
+            if(set.contains(nums[i])){
                 return true;
             } else {
-                map.put(nums[i],i);
+                set.add(nums[i]);
             }
         }
         return false;

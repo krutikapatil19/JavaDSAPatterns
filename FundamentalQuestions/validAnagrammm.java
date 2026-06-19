d226a9709454196b2ab8eb11cdd02bd3560b1f4d
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-19 12:29:53 +0530

    Valid Anagram.

---
+++ FundamentalQuestions/validAnagrammm.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+public class validAnagrammm{
+    public static boolean isAnagram(String s1, String s2){
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        if(s1.length()!= s2.length()){
+            return false;
+        }
+        for(int i = 0; i<s1.length(); i++){
+            if(map.containsKey(s1.charAt(i))){
+                map.put(s1.charAt(i),
+                map.getOrDefault(s1.charAt(i), 0)+1);
+            } else {
+                map.put(s1.charAt(i),1);
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        String s1 = "Eat";
+        String s2 = "Ate";
+        System.out.println(isAnagram(s1,s2));
+    }
+    
+}

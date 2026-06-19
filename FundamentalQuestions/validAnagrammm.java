92737a9c4788c53727273599b9709cd4c3e4d1d5
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-19 18:49:39 +0530

    Valid Anagram.

--- FundamentalQuestions/validAnagrammm.java
+++ FundamentalQuestions/validAnagrammm.java
@@ -6,18 +6,14 @@ public class validAnagrammm{
 
         s1 = s1.toLowerCase();
         s2 = s2.toLowerCase();
-        
+
         if(s1.length()!= s2.length()){
             return false;
         }
         for(int i = 0; i<s1.length(); i++){
-            if(map.containsKey(s1.charAt(i))){
                 map.put(s1.charAt(i),
-                map.getOrDefault(s1.charAt(i), 0)+1);
-            } else {
-                map.put(s1.charAt(i),1);
-            }
-        }
+                    map.getOrDefault(s1.charAt(i), 0)+1);
+                }
 
         for(int j = 0; j<s2.length();j++){
             if(!map.containsKey(s2.charAt(j))){

c0fdb74438cf0b2d3bfc61b5907c2b06392a284d
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-19 14:39:35 +0530

    Valid Anagram.

--- FundamentalQuestions/validAnagrammm.java
+++ FundamentalQuestions/validAnagrammm.java
@@ -29,7 +29,7 @@ public class validAnagrammm{
                 return false;
             }
         }
-        return false;
+        return true;
     }
     public static void main(String[] args){
         String s1 = "Eat";

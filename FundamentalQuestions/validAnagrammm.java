7f1365eb89d506974c3c97770fd3de561e359ff9
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-20 12:15:13 +0530

    Valid Anagram.

--- FundamentalQuestions/validAnagrammm.java
+++ FundamentalQuestions/validAnagrammm.java
@@ -2,14 +2,15 @@ import java.util.HashMap;
 public class validAnagrammm{
     public static boolean isAnagram(String s1, String s2){
 
+        if(s1.length()!= s2.length()){
+            return false;
+        }
+        
         HashMap<Character, Integer> map = new HashMap<>();
 
         s1 = s1.toLowerCase();
         s2 = s2.toLowerCase();
 
-        if(s1.length()!= s2.length()){
-            return false;
-        }
         for(int i = 0; i<s1.length(); i++){
                 map.put(s1.charAt(i),
                     map.getOrDefault(s1.charAt(i), 0)+1);

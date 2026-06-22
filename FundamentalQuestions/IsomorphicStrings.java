7a44ba2a46d1d039708b5ce8f5f11b9129e5c5ed
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-22 11:40:52 +0530

    Isomorphic Strings.

--- FundamentalQuestions/IsomorphicStrings.java
+++ FundamentalQuestions/IsomorphicStrings.java
@@ -1,31 +1,34 @@
 import java.util.HashMap;
-public class IsomorphicStrings {
-    public static boolean isIsomorphic(String s1, String s2){
 
-        if(s1.length() != s2.length()){
+public class IsomorphicStrings {
+    public static boolean isIsomorphic(String s1, String s2) {
+
+        if (s1.length() != s2.length()) {
             return false;
         }
 
-        HashMap<Character,Character>map = new HashMap<>();
+        HashMap<Character, Character> map = new HashMap<>();
 
-        for(int i = 0; i<s1.length(); i++){
+        for (int i = 0; i < s1.length(); i++) {
 
             char ch1 = s1.charAt(i);
             char ch2 = s2.charAt(i);
 
-            map.put(ch1,ch2);
-            
-            if(map.containsKey(ch1)){
-            if(map.get(ch1) != ch2){
-                return false;
+            if (map.containsKey(ch1)) {
+                // compare
+                if (map.get(ch1) != ch2) {
+                    return false;
+                }
+            } else {
+                map.put(ch1, ch2);
             }
-        }
         }
         return true;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         String s1 = "egg";
         String s2 = "add";
-        System.out.println(isIsomorphic(s1,s2));
+        System.out.println(isIsomorphic(s1, s2));
     }
 }

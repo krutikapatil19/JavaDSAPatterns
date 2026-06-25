36effe3a6af7721b34e9c75ceb96529ae235a25b
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-25 19:40:26 +0530

    Happy Number

--- FundamentalQuestions/HappyNumber.java
+++ FundamentalQuestions/HappyNumber.java
@@ -1,13 +1,22 @@
 import java.util.HashSet;
 public class HappyNumber {
+
+    //function to check whether a number is happy or not
     public static boolean isHappyNumber(int n){
 
         //happy number logic 
+        //HashSet stores all the sums to detect a repeating cycle
         HashSet<Integer> set = new HashSet<>();
+
+        //repeat until the number becomes 1 
         while(n != 1){
+
+            //if the number is already present , a cycle is found 
             if(set.contains(n)){
                 return false;
             }
+            //store the current number in the HashSet
+            else
             {
                 set.add(n);
                 n = findSquareSum(n);

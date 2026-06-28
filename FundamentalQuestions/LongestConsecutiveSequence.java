2bdaf8124288979d1f5739721230efb05495fc10
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-28 11:59:20 +0530

    Longest Consecutive Sequence.

--- FundamentalQuestions/LongestConsecutiveSequence.java
+++ FundamentalQuestions/LongestConsecutiveSequence.java
@@ -1,6 +1,21 @@
 public class LongestConsecutiveSequence {
     public static int longestConsecutiveSequencee(int[]nums){
 
+        //edge case
+        //sort the array
+        Arrays.sort(nums);
+
+        //variables 
+        int currentLength = 0;
+        int maxLength = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            //check if consecutive
+            if(nums[i]<nums[i+1]){
+
+            }
+
+        }
     }
     public static void main(String[] args){
         int[] nums = {100,4,200,1,3,2};

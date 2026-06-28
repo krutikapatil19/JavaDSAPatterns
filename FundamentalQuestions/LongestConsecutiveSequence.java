c57b7f6de2525fd720e4767fa43be0b44f095598
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-06-28 11:53:52 +0530

    Longest Consecutive Sequence.

---
+++ FundamentalQuestions/LongestConsecutiveSequence.java
@@ -0,0 +1,9 @@
+public class LongestConsecutiveSequence {
+    public static int longestConsecutiveSequencee(int[]nums){
+
+    }
+    public static void main(String[] args){
+        int[] nums = {100,4,200,1,3,2};
+        System.out.println(longestConsecutiveSequencee(nums));
+    }
+}

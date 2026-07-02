e66660b3f2c72626e96f8f58cc5c25473f7aa95d
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-02 17:57:15 +0530

    Longest Consecutive Sequence.

--- FundamentalQuestions/LongestConsecutiveSequence.java
+++ FundamentalQuestions/LongestConsecutiveSequence.java
@@ -1,31 +1,42 @@
 import java.util.HashSet;
 public class LongestConsecutiveSequence {
+
+    //function to find the length of the longest consecutive sequence
     public static int longestConsecutiveSequencee(int[]nums){
 
-        //edge case
+        //edge case: if array is empty , the answer is 0 
         if(nums.length == 0){
             return 0;
         }
         
+        //stores all numbers for fast lookup 
         HashSet<Integer> set = new HashSet<>();
-        //variables 
+        
+        //how many numbers counted so far (length of current sequence)
         int currentLength = 1;
+
+        //stores the maximum sequence length
         int maxLength = 1;
 
-        int current = 1;
+        //the number currently pointed at
+        int current;
 
-        //fill HashSet
+        //add all array elements into the HashSet
         for(int i = 0; i<nums.length; i++){
             set.add(nums[i]);
         }
+
+        //Traverse every number in the array
         for(int i = 0; i<nums.length;i++){
+
+            //check 
             if(!set.contains(nums[i]-1)){
                 current = nums[i];
                 currentLength = 1;
 
         //while the next consecutive number exists in HashSet, keep moving forward.
         while(set.contains(current+1)){
-            current = current++;
+            current++;
             currentLength++;
         }
             if(currentLength > maxLength){

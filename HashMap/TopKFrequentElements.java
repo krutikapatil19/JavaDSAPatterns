fa99a361c56a15c11bbf866555e04c57efe21a67
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-03 15:39:52 +0530

    Top K Frequent Elements.

---
+++ HashMap/TopKFrequentElements.java
@@ -0,0 +1,12 @@
+package HashMap;
+
+public class TopKFrequentElements {
+    public static int[] topFrequentElements(int[]nums,int k){
+
+    }
+    public static void main(String[] args){
+        int[]nums = {1,1,1,2,2,2,2,3};
+        int k = 2;
+        System.out.println(nums,k);
+    }
+}

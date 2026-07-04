1aff6e8cf2e7d90d854c871ca3721153b0c5efb2
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-04 17:43:00 +0530

    Valid Anagram.

--- HashMap/ValidAnagram.java
+++ HashMap/ValidAnagram.java
@@ -28,5 +28,6 @@ public class ValidAnagram {
     public static void main(String[] args){
         String s1 = "listen";
         String s2 = "silent";
+        System.out.println(isAnagram(s1,s2));
     }
 }

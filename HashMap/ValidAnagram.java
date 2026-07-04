8886b31df2e4766486ad5a0c8f035ecbc7ae6c7e
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-04 18:53:01 +0530

    Valid Anagram.

--- HashMap/ValidAnagram.java
+++ HashMap/ValidAnagram.java
@@ -2,14 +2,19 @@ import java.util.HashMap;
 public class ValidAnagram {
     public static boolean isAnagram(String s1 , String s2){
 
+        //if lengths of both strings are different , then they can't be anagrams
         if(s1.length()!=s2.length())return false;
 
+        //convert both strings to LowerCase
         s1 = s1.toLowerCase();
         s2 = s2.toLowerCase();
 
+        //Hashmap to store frequency of string characters
         HashMap <Character, Integer> map = new HashMap<>();
 
         for(char ch: s1.toCharArray()){
+
+            //to store the frequency of characters
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
 

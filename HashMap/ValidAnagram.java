5fbfcf10ecb8fc862f9f14bc5415bc6f00fae9ca
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-04 17:40:23 +0530

    Valid Anagram.

--- HashMap/ValidAnagram.java
+++ HashMap/ValidAnagram.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 public class ValidAnagram {
     public static boolean isAnagram(String s1 , String s2){
 
@@ -5,6 +6,18 @@ public class ValidAnagram {
 
         s1 = s1.toLowerCase();
         s2 = s2.toLowerCase();
+
+        HashMap <Character, Integer> map = new HashMap<>();
+
+        for(char ch: s1.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for(char ch : s2.toCharArray()){
+
+            if(!map.containsKey(ch)) return false;
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
 
     }
     public static void main(String[] args){

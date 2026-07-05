6e5643517a1db8f123aa635079bbdfbf2041f13a
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-05 15:59:50 +0530

    Valid Anagram.

---
+++ HashMap/GroupAnagrams.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.util.HashMap;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupingAnagrams(String[] strs){
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(int i = 0; i<strs.length; i++){
+
+            String word = strs[i];
+
+            char[]arr = word.toCharArray();
+
+            Arrays.sort(arr);
+
+            String sortedWord = new String(arr);
+
+            if(map.containsKey(sortedWord)) {
+
+                List<String>list = map.get(sortedWord);
+
+                list.add(word);
+             } else {
+                List<String> list = new ArrayList<>();
+
+                list.add(word);
+
+                map.put(sortedWord, list);
+             }
+            }
+             return new ArrayList<>(map.values());
+        }
+        public static void main(String[] args){
+            String[] strs = {"cat","dog","act","god"};
+            System.out.println(groupingAnagrams(strs));
+        }
+}

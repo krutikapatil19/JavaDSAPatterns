85e9fff35c2f49f3e06e5eacdb8c2aa9155f7c2e
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-06 15:00:04 +0530

    Isomorphic Strings.

--- HashMap/IsomorphicStrings.java
+++ HashMap/IsomorphicStrings.java
@@ -5,6 +5,18 @@ public class IsomorphicStrings {
         if(s1.length() != s2.length())return false;
 
         HashMap<Character, Character>map = new HashMap<>();
+
+        for(int i = 0; i<s1.length(); i++){
+
+            char ch1 = s1.charAt(i);
+            char ch2 = s2.charAt(i);
+
+            if(map.containsKey(ch1)){
+                if(map.get(ch1) != ch2) {
+                    return false;
+                }
+            }
+        }
     }
     public static void main(String[] args){
         String s1 = "egg";

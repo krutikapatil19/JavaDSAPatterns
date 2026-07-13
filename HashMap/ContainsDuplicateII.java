d0009b996489cb2f819e14d1c0ca0bc06f0a3187
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-13 22:07:39 +0530

    Contains Duplicate.

--- HashMap/ContainsDuplicateII.java
+++ HashMap/ContainsDuplicateII.java
@@ -9,11 +9,13 @@ public class ContainsDuplicateII {
 
         for(int i = 0; i<nums.length; i++){
             if(map.containsKey(nums[i])){
-                if(i-map.get(nums[i]) == k){
+                if(i-map.get(nums[i]) <= k){
                     return true;
                 }
             }
+            map.put(nums[i],i);
         }
+        return false;
     }
     public static void main(String[] args){
         int[] nums = {1,2,3,1};

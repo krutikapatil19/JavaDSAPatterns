9d0c56372676be21f8bfd944e10f2908cbdb6e33
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-13 22:15:14 +0530

    Contains Duplicate.

--- HashMap/ContainsDuplicateII.java
+++ HashMap/ContainsDuplicateII.java
@@ -2,16 +2,24 @@ import java.util.HashMap;
 public class ContainsDuplicateII {
     public static boolean containsNearbyDuplicate(int[]nums, int k){
 
+        //To store : Numbers - > Last index they were seen
         HashMap<Integer,Integer>map = new HashMap<>();
 
+        //Traverse the array 
         for(int i = 0; i<nums.length; i++){
+
+            //if the number was seen before
             if(map.containsKey(nums[i])){
+
+                //Check if the distance is at most k 
                 if(i-map.get(nums[i]) <= k){
                     return true;
                 }
             }
+            //update the latest index of the current number 
             map.put(nums[i],i);
         }
+        //no valid fair found
         return false;
     }
     public static void main(String[] args){

bc4e38ce0ebd1cc7d4ef3bee02e35f53d3abde57
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-13 21:57:20 +0530

    Contains Duplicate.

--- HashMap/ContainsDuplicateII.java
+++ HashMap/ContainsDuplicateII.java
@@ -2,6 +2,10 @@ package HashMap;
 
 public class ContainsDuplicateII {
     public static boolean containsNearbyDuplicate(int[]nums, int k){
-        
+
+    }
+    public static void main(String[] args){
+        int[] nums = {};
+        int k = 3;
     }
 }

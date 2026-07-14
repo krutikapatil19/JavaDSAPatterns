8c34af83460657e13dad0b430c92af71fffaf718
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-14 13:28:09 +0530

    Maximum Average Subarray I.

--- Sliding Window/MaxAvgSubarrayI.java
+++ Sliding Window/MaxAvgSubarrayI.java
@@ -1,3 +1,9 @@
 public class MaxAvgSubarrayI {
-    
+    public static int maxAverageSubarray(int[]nums, int k){
+
+    }
+    public static void main(String[] args){
+        int[]nums = {};
+        int k;
+    }
 }

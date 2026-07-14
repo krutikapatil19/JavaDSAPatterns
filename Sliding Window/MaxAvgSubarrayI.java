b9efc21f7ec239b9a3fa5389e8f21c697403aac5
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-14 13:31:39 +0530

    Maximum Average Subarray I.

--- Sliding Window/MaxAvgSubarrayI.java
+++ Sliding Window/MaxAvgSubarrayI.java
@@ -1,9 +1,15 @@
 public class MaxAvgSubarrayI {
     public static int maxAverageSubarray(int[]nums, int k){
 
+        int start = 0;
+        int end = 0;
+
+        //take the prev sum -> remove the element leaving the window - add the element entering the window
+        int currentSum = currentSum - nums[start] + nums[end];
     }
     public static void main(String[] args){
         int[]nums = {};
         int k;
+        System.out.println(maxAverageSubarray(nums,k));
     }
 }

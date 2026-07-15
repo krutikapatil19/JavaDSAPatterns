5de43c1997f2856f977eada1f1293e3a7a69b4b0
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-15 22:03:22 +0530

    Maximum Sum Subarray of Size K.

--- Sliding Window/MaximumSumSubarrayOfSizeK.java
+++ Sliding Window/MaximumSumSubarrayOfSizeK.java
@@ -1,8 +1,12 @@
 public class MaximumSumSubarrayOfSizeK {
     public static int maxSubarraySum(int[]nums, intk){
 
+
+    return ;
     }
     public static void main(String[] args){
-        
+        int[] nums = {};
+        int k ;
+        System.out.println(maxSubarraySum(nums,k));
     }
 }

738845e11afee408e19981d97fea3c260a130f96
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-17 22:48:35 +0530

    Longest Substring Without Repeating Characters.

--- Sliding Window/LongestSubstringWoRepeatingChar.java
+++ Sliding Window/LongestSubstringWoRepeatingChar.java
@@ -1,6 +1,10 @@
+import java.util.HashSet;
 public class LongestSubstringWoRepeatingChar {
     public static int longestSubstring(String s){
 
+        for(int i = 0; i<s.length();i++){
+
+        }
     }
     public static void main(String[] args){
         String s = "bbbbac";

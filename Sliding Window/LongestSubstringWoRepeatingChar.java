ec305ab8d094770accb21e48e91d3f917717b65f
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-18 18:50:19 +0530

    Longest Substring Without Repeating Characters.

--- Sliding Window/LongestSubstringWoRepeatingChar.java
+++ Sliding Window/LongestSubstringWoRepeatingChar.java
@@ -2,18 +2,26 @@ import java.util.HashSet;
 public class LongestSubstringWoRepeatingChar {
     public static int lengthOfLongestSubstring(String s){
 
+        //Stores unique characters in the current window 
         HashSet<Character> set = new HashSet<>();
-        int left = 0;
-        int right = 0;
-        int maxLength = 0;
+        
+        int left = 0;   //left pointer of the window
+        int right = 0;  //right pointer of the window
+        int maxLength = 0;  //stores the maximum length found
 
+        //Expand the window using the right pointer
         for(right = 0; right<s.length();right++){
+
+            //stores the current character
             char ch = s.charAt(right);
+
+            //as long as the window is valid, keep shrinking it from the left
             while(set.contains(ch)){
                 set.remove(s.charAt(left));
                 left++;
             }
-            set.add(s.charAt(right));
+            //add the current character after the window becomes valid. (which is at the right)
+            set.add(ch);
             maxLength = Math.max(maxLength,right-left+1);
         }
         return maxLength;

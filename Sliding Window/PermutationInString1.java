9d31864b409e4e6b70ff3ca575ae7777d08b641a
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-21 22:55:14 +0530

    Longest Repeating Character Replacement.

--- Sliding Window/PermutationInString1.java
+++ Sliding Window/PermutationInString1.java
@@ -1,3 +1,10 @@
 public class PermutationInString1 {
-    
+    public static boolean isPermutation(String s , String t){
+
+    }
+    public static void main(String[] args){
+        String s = "";
+        String t = "";
+        System.out.println(isPermutation(s,t));
+    }
 }

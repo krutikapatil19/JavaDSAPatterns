670177e98145cf8e32491991bf62316a5e93a8ec
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-21 12:51:05 +0530

    Longest Repeating Character Replacement.

--- Sliding Window/Longest.java
+++ Sliding Window/Longest.java
@@ -13,7 +13,22 @@ public class Longest {
             int count = map.getOrDefault(ch,0)+1;
             map.put(ch, count);
 
+            //update max Frequency , if this count is the new highest
             maxFreq = Math.max(maxFreq, count);
+
+            int windowLength = right - left + 1;
+
+            while(windowLength - maxFreq > k){
+                char leftChar = s.charAt(left);
+
+                //decrease leftchar's count in the map
+                map.put(leftChar, map.get(leftChar)-1);
+                left++;
+
+                //recalculate windowLength
+                windowLength =  right-left+1;
+                //by doing right-left+1
+            }
           
         }
         return maxLength;

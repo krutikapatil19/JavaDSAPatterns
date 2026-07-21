6a14871ba50b973e5be64f9a6c95cf82c70317b1
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-21 13:29:05 +0530

    Longest Repeating Character Replacement.

--- Sliding Window/Longest.java
+++ Sliding Window/Longest.java
@@ -4,31 +4,36 @@ public class Longest {
 
         HashMap<Character,Integer>map = new HashMap<>();
         int left=0;
-        int maxFreq = 0;
-        int maxLength = 0;
+        int maxFreq = 0;    //highest freq of any character in current window
+        int maxLength = 0;  //store the result
 
+        //Expand the window using right pointer
         for(int right = 0; right<s.length(); right++){
             char ch = s.charAt(right);
             
+            //Increase freq of current character
             int count = map.getOrDefault(ch,0)+1;
             map.put(ch, count);
 
-            //update max Frequency , if this count is the new highest
+            //update max Frequency , if this count is the new highest (updating highest freq seen in this window)
             maxFreq = Math.max(maxFreq, count);
 
+            //current window size
             int windowLength = right - left + 1;
 
+            //If more than k replacements are needed, that means the window is invalid -> shrink the window from left 
             while(windowLength - maxFreq > k){
                 char leftChar = s.charAt(left);
 
-                //decrease leftchar's count in the map
+                //decrease leftchar's count in the map (as the leftmost char is removed )
                 map.put(leftChar, map.get(leftChar)-1);
                 left++;
 
-                //recalculate windowLength
+                //recalculate windowLength after shrinking
                 windowLength =  right-left+1;
-                //by doing right-left+1
             }
+            
+            //store the maximum valid window length
             maxLength = Math.max(maxLength, windowLength);
           
         }

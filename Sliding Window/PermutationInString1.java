22e080faf854c93d4bd14b97247fb2dfc621a80e
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-22 11:04:15 +0530

    Permutation In String.

--- Sliding Window/PermutationInString1.java
+++ Sliding Window/PermutationInString1.java
@@ -2,6 +2,14 @@ public class PermutationInString1 {
     public static boolean isPermutation(String s , String t){
 
         int windowSize = s.length();
+
+        //Base Case : if s length is greater than t , then t can never contain the permutation of s (as its larger than s)
+        if(s.length()>t.length()){
+            return false;
+        }
+
+        //frequency array
+        int[] frequency = new int[26];
         }
     public static void main(String[] args){
         String s = "ab";

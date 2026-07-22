4c8a0b0d21d1ce453ec340a6a450c816790dea0c
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-22 10:51:43 +0530

    Permutation In String.

--- Sliding Window/PermutationInString1.java
+++ Sliding Window/PermutationInString1.java
@@ -1,10 +1,11 @@
 public class PermutationInString1 {
     public static boolean isPermutation(String s , String t){
 
-    }
+        int windowSize = s.length();
+        }
     public static void main(String[] args){
-        String s = "";
-        String t = "";
+        String s = "ab";
+        String t = "eidbaooo";
         System.out.println(isPermutation(s,t));
     }
 }

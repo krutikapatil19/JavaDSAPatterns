bf5a913dcb31e94985ca3c1d4d57f0066a968363
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-22 12:11:11 +0530

    Permutation In String.

--- Sliding Window/PermutationInString1.java
+++ Sliding Window/PermutationInString1.java
@@ -3,13 +3,19 @@ public class PermutationInString1 {
 
         int windowSize = s.length();
 
-        //Base Case : if s length is greater than t , then t can never contain the permutation of s (as its larger than s)
+        //Base Case : if s is longer than t , a permutation is impossible.
         if(s.length()>t.length()){
             return false;
         }
 
         //create frequency array
-        int[] frequency = new int[26];
+        int[] sFreq = new int[26];      //stores the freq of characters of s
+        int[] windowFreq = new int[26]; //stores the freq of characters in current window of t. //This array keeps changing as the window slides 
+
+        for(int i = 0; i<windowSize; i++){
+            //update s freq
+            //update window freq
+        }
         }
     public static void main(String[] args){
         String s = "ab";

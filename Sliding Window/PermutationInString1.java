60dcfaa0df4af45d4e394ddbe7f8b7f3aeaf6365
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-22 13:34:04 +0530

    Permutation In String.

--- Sliding Window/PermutationInString1.java
+++ Sliding Window/PermutationInString1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class PermutationInString1 {
     public static boolean isPermutation(String s , String t){
 
@@ -13,9 +14,27 @@ public class PermutationInString1 {
         int[] windowFreq = new int[26]; //stores the freq of characters in current window of t. //This array keeps changing as the window slides 
 
         for(int i = 0; i<windowSize; i++){
-            //update s freq
-            //update window freq
+
+            //count char of s
+            sFreq[s.charAt(i)-'a']++;
+
+            //count characters of first window of t
+            windowFreq[t.charAt(i)-'a']++;
         }
+        //Comparison of both Arrays
+            if(Arrays.equals(sFreq,windowFreq)){
+                return true;
+            }
+
+            for(int i = windowSize; i<t.length(); i++){
+                windowFreq[t.charAt(i-windowSize)- 'a']--;
+                windowFreq[t.charAt(i)-'a']++;
+            
+                if(Arrays.equals(sFreq, windowFreq)){
+                    return true;
+                }
+            }
+        return false;
         }
     public static void main(String[] args){
         String s = "ab";

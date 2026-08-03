523e3b1e3aeb2ae9180dcb511be4acb631eb77df
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-08-03 22:59:09 +0530

    Insert Search Position.

--- BinarySearch/InsertSearchPosition.java
+++ BinarySearch/InsertSearchPosition.java
@@ -15,10 +15,11 @@ public class InsertSearchPosition {
                 left=mid+1;
             } 
         }
+        return left;
     }
     public static void main(String[]args){
         int[]nums = {1,3,4,5,6};
         int target = 2;
-        System.out.println(nums,target);
+        System.out.println(searchInsert(nums,target));
     }
 }

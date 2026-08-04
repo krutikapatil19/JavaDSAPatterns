b853bb5c2bf62d6b919b0b4cbb3132beaa359155
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-08-04 21:28:23 +0530

    Find search range.

--- BinarySearch/searchRange.java
+++ BinarySearch/searchRange.java
@@ -1,15 +1,23 @@
 public class searchRange {
+
+
     public static int[] findSearchRange(int[]nums, int k){
+        int[] result = new int[2];
+
+        return result;
+    }
+
+    public static int findBound(int[]nums, int target, boolean isSearchingLeft){
 
         int left = 0;
         int right = nums.length-1;
 
-        int[] result = new int[2];
         while(left<=right){
-
+            //main logic
         }
-        return result;
+        return -1;
     }
+        
     public static void main(String[]args){
         int[]nums = {5,7,7,8,8,8,10};
         int k = 8;

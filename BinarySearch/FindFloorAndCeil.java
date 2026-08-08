2d42a664849fd964f0a7a93a253522a2a459b704
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-08-08 22:21:50 +0530

    Find Floor and Ceil of a Number in a Sorted Array.

---
+++ BinarySearch/FindFloorAndCeil.java
@@ -0,0 +1,10 @@
+public class FindFloorAndCeil {
+    public static int[] floorAndCeilOfANumber(int[]nums, int k){
+
+    }
+    public static void main(String[] args){
+        int[] nums = {1, 2, 8, 10, 10, 12, 19};
+        int x = 5;
+        System.out.println(floorAndCeilOfANumber(nums,k));
+    }
+}

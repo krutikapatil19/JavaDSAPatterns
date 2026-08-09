d42f1faf44e957b7f4948fcd5cbcc19076151f26
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-08-09 13:12:19 +0530

    Search Target element in Rotated Sorted Array.

--- BinarySearch/SearchInRotatedSortedArray.java
+++ BinarySearch/SearchInRotatedSortedArray.java
@@ -20,6 +20,14 @@ public class SearchInRotatedSortedArray {
                 //else ,the target must be in the right half.
                 left = mid+1;
             }
+            }else {
+                //when the right half is sorted 
+                //if target is between nums[mid] and nums[right] 
+                if(nums[mid]<=target && target<=nums[right]){
+                    left=mid+1;
+                } else {
+                    right = mid-1;
+                }
             }
         }
         return ans;

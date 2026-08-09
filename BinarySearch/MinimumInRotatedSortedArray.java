fa54ad2c22c5a156445f8fa71b583ccbe3a71356
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-08-09 19:23:54 +0530

    Minimum in a Rotated Sorted Array.

---
+++ BinarySearch/MinimumInRotatedSortedArray.java
@@ -0,0 +1,22 @@
+public class MinimumInRotatedSortedArray {
+    public static int findMinimum(int[] nums){
+
+        int left = 0;
+        int right = nums.length-1;
+        int minimum=Integer.MAX_VALUE;
+
+        while(left<=right){
+            int mid = (left+right)/2;
+
+            //checking which half is sorted
+            if(nums[left]<=mid){
+                minimum = nums[left];
+            }
+        }
+        return minimum;
+    }
+    public static void main(String[] args){
+        int[] nums = {3,4,5,1,2};
+        System.out.println(nums);
+    }
+}

bcda6334ca941f7774f3a047ecee7a7ecd4af138
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-08-19 23:58:07 +0530

    Minimum in a Rotated Sorted Array.

--- BinarySearch/MinimumInRotatedSortedArrayy.java
+++ BinarySearch/MinimumInRotatedSortedArrayy.java
@@ -18,7 +18,7 @@ public class MinimumInRotatedSortedArrayy {
                 left=mid+1;
             }
         }
-    return;
+    return left;
     }
     public static void main(String[] args){
         int[]nums = {3,4,5,1,2};

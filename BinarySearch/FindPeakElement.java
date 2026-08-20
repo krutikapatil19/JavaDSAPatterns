2a145b9f29bb967c765c38074862f094d53afc77
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-08-20 23:31:15 +0530

    Find peak Element.

---
+++ BinarySearch/FindPeakElement.java
@@ -0,0 +1,8 @@
+public class FindPeakElement {
+    public static int findpeak(int[]nums,int target){
+
+    }
+    public static void main(String[] args){
+        
+    }
+}
--- BinarySearch/MinimumInRotatedSortedArrayy.java
+++ BinarySearch/MinimumInRotatedSortedArrayy.java
@@ -22,6 +22,7 @@ public class MinimumInRotatedSortedArrayy {
     }
     public static void main(String[] args){
         int[]nums = {4,5,6,7,0,1,2};
+        
         System.out.println(findMinimum(nums));
     }
 }

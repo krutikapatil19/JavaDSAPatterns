40190235661c1a3461b016367c860b505111faea
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-08-23 23:34:02 +0530

    Find peak Element.

--- BinarySearch/FindPeakElement.java
+++ BinarySearch/FindPeakElement.java
@@ -14,6 +14,7 @@ public class FindPeakElement {
             right = mid;
         }
     }
+    return left;
     }
     public static void main(String[] args){
         int[] nums = {1,2,1,3,5,6,4};
